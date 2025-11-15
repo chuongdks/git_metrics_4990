321caed84181137cb9b90b8593dea1550fca3e64
Hoang Chuong Pham <pham75@uwindsor.ca>
2025-11-14 20:59:38 -0500

    # Commit 1.1.1: Prepare to add rate limit to the API

---
+++ rate_limit_check/rate_limit_git_API.py
@@ -0,0 +1,20 @@
+import requests
+from dotenv import load_dotenv
+import os
+import json
+load_dotenv("./api_key.env")
+GITHUB_TOKEN = os.getenv("GITHUB_API_KEY")
+
+headers = {
+    'Authorization': f'Bearer {GITHUB_TOKEN}',
+}
+
+response = requests.get('https://api.github.com/rate_limit', headers=headers)
+
+data = response.json()
+
+# Pretty-print and save to file
+with open("./rate_limit_check/rate_limit.json", "x") as f:
+    json.dump(data, f, indent=4)
+
+print("Saved to rate_limit.json")
